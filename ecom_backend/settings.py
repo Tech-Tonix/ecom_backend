09dcd0dd2f5b9ba4174eaa899f1a7e26f4f38115
MalakBazine <m.bazine@esi-sba.dz>
2023-05-08 14:56:19 +0100

    products images upload

--- ecom_backend/settings.py
+++ ecom_backend/settings.py
@@ -29,6 +29,8 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Application definition
 
