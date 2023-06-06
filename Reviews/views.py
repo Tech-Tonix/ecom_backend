ce2467e0a6e52bbb6943c5d32500ba5aa4e42a77
MalakBazine <m.bazine@esi-sba.dz>
2023-06-06 14:58:45 +0100

    fixing reviews

--- Reviews/views.py
+++ Reviews/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-
+from rest_framework.permissions import IsAdminUser , IsAuthenticated
 from rest_framework import status,generics,mixins,viewsets
-  #filters
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView ,RetrieveUpdateDestroyAPIView
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.exceptions import NotFound , ValidationError
@@ -11,7 +10,7 @@ from rest_framework import permissions
 from .serializers import ReviewSerializer
 from .models import Review
 from Store.models import Product
-
+from rest_framework.exceptions import PermissionDenied
 
 # Create your views here.
 class ReviewListAPIView(generics.ListAPIView):
@@ -22,6 +21,7 @@ class ReviewListAPIView(generics.ListAPIView):
     
 class ReviewCreateAPIView(generics.CreateAPIView):
     serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
     queryset = Review.objects.all()
     def perform_create(self,serializer):
         pk = self.kwargs.get('pk')
@@ -44,3 +44,13 @@ class ReviewCreateAPIView(generics.CreateAPIView):
 class ReviewRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
+
+
+    def perform_destroy(self, instance):
+        user = self.request.user
+
+        if instance.user != user:
+            raise PermissionDenied("You are not allowed to delete this review.")
+
+        instance.delete()
