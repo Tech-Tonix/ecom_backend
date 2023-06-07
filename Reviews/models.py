1d2b0dd22e1d361752c7303dda2c3c8630078403
MalakBazine <m.bazine@esi-sba.dz>
2023-06-07 15:55:29 +0100

    review serializer update

--- Reviews/models.py
+++ Reviews/models.py
@@ -7,7 +7,9 @@ class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     content = models.TextField()
+    rating = models.PositiveIntegerField(blank=True, null=True)
     active = models.BooleanField(default=True)
+
     # created_at = models.DateTimeField(auto_now_add=True)
     # updated_at = models.DateTimeField(auto_now=True)
     
